53b965fec101c4feaffd49837a2652f1218fef85
Dmitriy Alhimovich <dmitriy.alhimovich@arcadia.spb.ru>
2021-06-24 15:35:24 +0300

    Add project files.

---
+++ WebApiControllerApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace WebApiControllerApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WebHost.CreateDefaultBuilder()
+                .ConfigureServices(services => services.AddMvc())
+                .Configure(app => app.UseMvc())
+                .Build()
+                .Run();
+
+        }
+    }
+}
---
+++ WebApiControllerApp/StudentsController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApiControllerApp
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class StudentsController
+    {
+        [HttpGet]
+        public IEnumerable<Student> GetAll()
+        {
+            var students = StudentsRepository.Students;
+            return students;
+        }
+
+        [HttpGet("{name}")]
+        public Student GetByName(string name)
+        {
+            
+            var student = StudentsRepository.Students.FirstOrDefault(s => s.Name == name);
+            return student;
+        }
+
+        [HttpPost]
+        public Student Add([FromBody]Student student)
+        {
+            StudentsRepository.Students.Add(student);
+            return student;
+        }
+    }
+
+    public class Student
+    {
+        public string Name { get; set; }
+        public DateTime BirthDate { get; set; }
+        public int Course { get; set; }
+        public string Group { get; set; }
+    }
+
+    public class StudentsRepository
+    {
+        public static List<Student> Students =>
+            new()
+            {
+                new Student { Name = "Alex", Course = 2 },
+                new Student { Name = "Max", Course = 3 },
+                new Student { Name = "Eugene", Course = 1 },
+            };
+    }
+}
